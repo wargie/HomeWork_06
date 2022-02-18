de5322193c40638a79c8a1419d578b3de5045d64
wargie <wargie@bk.ru>
2022-02-18 06:49:32 +0200

    оптимизирован код задачи №42

--- Example_42/Program.cs
+++ Example_42/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 42.Определить сколько чисел больше 0 введено с клавиатуры
+
+Console.Write("Введите колличество чисел: ");
+int volume = int.Parse(Console.ReadLine());
+int count = 0;
+
+for (int i = 0; i < volume; i++)
+    
+    {
+        Console.Write($"Введите число {i + 1}: ");
+        int x = int.Parse(Console.ReadLine());
+        if (x > 0)
+            count++;
+    }
+
+Console.WriteLine($"С клавиатуры введено {count} чисел больше ноля");
