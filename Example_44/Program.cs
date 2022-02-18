578ab40d8deaf95ae3c9ffd4327c42d1529b43dc
wargie <wargie@bk.ru>
2022-02-18 13:13:18 +0200

    Поиск ошибок в коде к задаче №44

--- Example_44/Program.cs
+++ Example_44/Program.cs
@@ -14,7 +14,6 @@ Console.Write("Введите значение k2: ");
 var k2 = int.Parse(Console.ReadLine());
 
 int x = (b2 - b1)/(k1 - k2);
-
 int y = (k1 * (b2 - b1)/(k1 - k2) + b1);
 
 Console.WriteLine($"Координата точки пересечения прямых равна {x} и {y}");
